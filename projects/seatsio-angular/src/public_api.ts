322cff37c1531c3ac3f65f624e1bd4adf6ced200
Nahuel Chaves <nahuel.chaves@gmail.com>
2019-01-21 11:39:02 -0300

    cleanup

--- projects/seatsio-angular/src/public_api.ts
+++ projects/seatsio-angular/src/public_api.ts
@@ -2,6 +2,6 @@
  * Public API Surface of seatsio-angular
  */
 
-export * from './lib/seatsio-angular.service';
+export * from './lib/seatsio.service';
 export * from './lib/seatsio-designer/seatsio-designer.component';
 export * from './lib/seatsio-angular.module';
