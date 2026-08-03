48c0330f88e8cdd4f03c4e0a10795f613086bc59
mroloux <matti@seats.io>
2026-08-03 15:04:10 +0200

    chore: simplify ESLint configuration by removing redundant rules

--- projects/seatsio-angular/eslint.config.js
+++ projects/seatsio-angular/eslint.config.js
@@ -1,32 +1,3 @@
 // @ts-check
-const { defineConfig } = require("eslint/config");
-const rootConfig = require("../../eslint.config.js");
-
-module.exports = defineConfig([
-  ...rootConfig,
-  {
-    files: ["**/*.ts"],
-    rules: {
-      "@angular-eslint/directive-selector": [
-        "error",
-        {
-          type: "attribute",
-          prefix: "si",
-          style: "camelCase",
-        },
-      ],
-      "@angular-eslint/component-selector": [
-        "error",
-        {
-          type: "element",
-          prefix: "si",
-          style: "kebab-case",
-        },
-      ],
-    },
-  },
-  {
-    files: ["**/*.html"],
-    rules: {},
-  }
-]);
+// The root config already uses the "si" prefix, so no overrides are needed here.
+module.exports = require("../../eslint.config.js");
--- projects/testApp/eslint.config.js
+++ projects/testApp/eslint.config.js
@@ -25,8 +25,4 @@ module.exports = defineConfig([
       ],
     },
   },
-  {
-    files: ["**/*.html"],
-    rules: {},
-  }
 ]);
