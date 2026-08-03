20888289b98f6f327949e76b05006a08a00f73ae
mroloux <matti@seats.io>
2026-08-03 14:49:23 +0200

    Revert "chore: upgrade Angular to version 22"
    This reverts commit cb8853ad5b73fb3e2689bd03e378977b7aaaaa69.

--- projects/testApp/src/app/app.component.ts
+++ projects/testApp/src/app/app.component.ts
@@ -1,13 +1,11 @@
-import {Component, ChangeDetectionStrategy} from '@angular/core';
+import {Component} from '@angular/core';
 import {ChartDesignerConfigOptions, ChartRendererConfigOptions, EventManagerConfigOptions} from '@seatsio/seatsio-types';
 import {EmbeddableProps} from 'seatsio-angular';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css'],
-    changeDetection: ChangeDetectionStrategy.Eager,
-    standalone: false
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Seatsio-angular test page';
@@ -28,9 +26,11 @@ export class AppComponent {
     chartJsUrl: 'https://cdn-staging-{region}.seatsio.net/chart.js',
     region: 'eu',
     onRenderStarted: () => {
+      // tslint:disable-next-line:no-console
       console.info('Render Started');
     },
     onChartRendered: () => {
+      // tslint:disable-next-line:no-console
       console.info('Render Finished');
     },
     priceFormatter: price => ('$' + price)
@@ -43,6 +43,7 @@ export class AppComponent {
     event: 'fullExampleWithoutSectionsEvent',
     mode: 'manageObjectStatuses',
     onChartRendered: () => {
+      // tslint:disable-next-line:no-console
       console.info('Render Finished');
     }
   })
